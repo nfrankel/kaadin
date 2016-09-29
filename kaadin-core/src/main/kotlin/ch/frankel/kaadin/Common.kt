85a763e3f007a6479c9be36aa18dffe24ea16bfa
Nicolas Frankel <nicolas@frankel.ch>
2016-09-29 19:05:32 +0200

    Introduce proper Width and Height abstractions

--- kaadin-core/src/main/kotlin/ch/frankel/kaadin/Common.kt
+++ kaadin-core/src/main/kotlin/ch/frankel/kaadin/Common.kt
@@ -4,9 +4,11 @@ import com.vaadin.server.*
 
 fun Sizeable.height(height: String) = setHeight(height)
 fun Sizeable.height(height: Float, unit: Sizeable.Unit) = setHeight(height, unit)
+fun Sizeable.height(height: Height) = setHeight(height.size, height.unit)
 fun Sizeable.heightUndefined() = setHeightUndefined()
 fun Sizeable.width(width: String) = setWidth(width)
 fun Sizeable.width(width: Float, unit: Sizeable.Unit) = setWidth(width, unit)
+fun Sizeable.width(width: Width) = setWidth(width.size, width.unit)
 fun Sizeable.widthUndefined() = setWidthUndefined()
 fun Sizeable.size(width: String, height: String) {
     width(width)
@@ -20,5 +22,13 @@ fun Sizeable.size(width: Float, widthUnit: Sizeable.Unit, height: Float, heightU
     width(width, widthUnit)
     height(height, heightUnit)
 }
+fun Sizeable.size(width: Width, height: Height) {
+    width(width)
+    height(height)
+}
 fun Sizeable.sizeUndefined() = setSizeUndefined()
 fun Sizeable.sizeFull() = setSizeFull()
+
+open class Size(val size: Float, val unit: Sizeable.Unit)
+class Height(size: Float, unit: Sizeable.Unit) : Size(size, unit)
+class Width(size: Float, unit: Sizeable.Unit) : Size(size, unit)
