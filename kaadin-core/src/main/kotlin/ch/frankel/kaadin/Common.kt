58bd27c538f5a139e4914355248d0fa426f5f561
Nicolas Frankel <nicolas@frankel.ch>
2016-10-07 18:07:36 +0200

    Put size-related functions into dedicated file

--- kaadin-core/src/main/kotlin/ch/frankel/kaadin/Common.kt
+++ kaadin-core/src/main/kotlin/ch/frankel/kaadin/Common.kt
@@ -18,37 +18,6 @@ package ch.frankel.kaadin
 import com.vaadin.server.*
 import java.net.URI
 
-fun Sizeable.height(height: String) = setHeight(height)
-fun Sizeable.height(height: Float, unit: Sizeable.Unit) = setHeight(height, unit)
-fun Sizeable.height(height: Height) = setHeight(height.size, height.unit)
-fun Sizeable.heightUndefined() = setHeightUndefined()
-fun Sizeable.width(width: String) = setWidth(width)
-fun Sizeable.width(width: Float, unit: Sizeable.Unit) = setWidth(width, unit)
-fun Sizeable.width(width: Width) = setWidth(width.size, width.unit)
-fun Sizeable.widthUndefined() = setWidthUndefined()
-fun Sizeable.size(width: String, height: String) {
-    width(width)
-    height(height)
-}
-fun Sizeable.size(width: Float, height: Float, unit: Sizeable.Unit) {
-    width(width, unit)
-    height(height, unit)
-}
-fun Sizeable.size(width: Float, widthUnit: Sizeable.Unit, height: Float, heightUnit: Sizeable.Unit) {
-    width(width, widthUnit)
-    height(height, heightUnit)
-}
-fun Sizeable.size(width: Width, height: Height) {
-    width(width)
-    height(height)
-}
-fun Sizeable.sizeUndefined() = setSizeUndefined()
-fun Sizeable.sizeFull() = setSizeFull()
-
-open class Size(val size: Float, val unit: Sizeable.Unit)
-class Height(size: Float, unit: Sizeable.Unit) : Size(size, unit)
-class Width(size: Float, unit: Sizeable.Unit) : Size(size, unit)
-
 fun page(init: Page.() -> Unit) = Page.getCurrent().apply(init)
 fun location(uri: String) = page { setLocation(uri) }
 fun location(uri: URI) = page { setLocation(uri) }
